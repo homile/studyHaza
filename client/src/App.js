a412b5eaccbec49662ead972798332a3accdd0a9
Beanxx <tnqls991211@naver.com>
2022-07-19 21:47:52 +0900

    style: GlobalStyles-font 다시 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import GlobalStyles from "./GlobalStyles";
 import "./App.css";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import "./fonts/fonts.css";
+
+const GlobalStyles = createGlobalStyle`
+  .app {font-family: "Pretendard-Bold";}
+`;
+
+const HeadLine = styled.h1`
+  font-family: "Pretendard-Bold";
+`;
 
 function App() {
   return (
     <div className="app">
       <GlobalStyles />
-      <h1>App</h1>
+      <HeadLine>App</HeadLine>
     </div>
   );
 }
