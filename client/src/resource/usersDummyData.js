66330c9da6df7582742b49e143e3bf691b4b68d4
homile <ssanho2@naver.com>
2022-07-19 21:52:34 +0900

    feat: users, posts 더미 데이터 추가

---
+++ client/src/resource/usersDummyData.js
@@ -0,0 +1,32 @@
+const users = [
+	{
+		num: 1,
+		userEmail: 'alsalsdyd@gmail.com',
+		userPassword: '!1234',
+		nickName: '조민우',
+		creationDate: 'new Date()', // korea time 
+	},
+	{
+		num: 2,
+		userEmail: 'abc@gamil.com',
+		userPassword: '@1234',
+		nickName: '조진우',
+		creationDate: 'new Date()', // korea time 
+	},
+	{
+		num: 3,
+		userEmail: 'dse@naver.com',
+		userPassword: '4321!',
+		nickName: '말괄량이',
+		creationDate: 'new Date()', // korea time 
+	},
+	{
+		num: 4,
+		userEmail: 'jin@gmail.com',
+		userPassword: '!1234',
+		nickName: 'chapchap',
+		creationDate: 'new Date()', // korea time 
+	},
+]
+
+export default users;
