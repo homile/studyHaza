290a35b2725ceaee09e056bd3cbea18eafecdd4c
Beanxx <tnqls991211@naver.com>
2022-07-22 01:00:12 +0900

    style: GlobalStyle - font 지정

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,32 @@
 import React from "react";
 import "./App.css";
 import styled from "styled-components";
-import { createGlobalStyle } from "styled-components";
-import reset from 'styled-reset'
-import "./fonts/fonts.css";
+// import { createGlobalStyle } from "styled-components";
+// import reset from "styled-reset";
+// import "./fonts/fonts.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// const GlobalStyles = createGlobalStyle`
+//   ${reset}
+//   .app {font-family: "Pretendard-Bold";}
+// `;
 
-const GlobalStyles = createGlobalStyle`
-  ${reset}
-  .app {font-family: "Pretendard-Bold";}
-`;
-
-const HeadLine = styled.h1`
-  font-family: "Pretendard-Bold";
-`;
+const HeadLine = styled.h1``;
 
 function App() {
   return (
     <div className="app">
-      <GlobalStyles />
-      <HeadLine>App</HeadLine>
+      <Nav />
+      <HeadLine>StudyHaza</HeadLine>
       <Router>
         <Routes>
-          <Route path="/"  /* element={이동할컴포넌트} */ />
-          <Route path="/login"  /* element={이동할컴포넌트} */ />
-          <Route path="signup/"  /* element={이동할컴포넌트} */ />
-          <Route path="/board"  /* element={이동할컴포넌트} */ />
-          <Route path="/studyboard"  /* element={이동할컴포넌트} */ />
-          <Route path="/mypage"  /* element={이동할컴포넌트} */ />
+          <Route path="/" /* element={이동할컴포넌트} */ />
+          <Route path="/login" /* element={이동할컴포넌트} */ />
+          <Route path="signup/" /* element={이동할컴포넌트} */ />
+          <Route path="/board" /* element={이동할컴포넌트} */ />
+          <Route path="/studyboard" /* element={이동할컴포넌트} */ />
+          <Route path="/mypage" /* element={이동할컴포넌트} */ />
         </Routes>
       </Router>
     </div>
