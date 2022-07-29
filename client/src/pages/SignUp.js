161c72ecbc63571b6fabde18b0e5640f5d969d42
homile <ssanho2@naver.com>
2022-07-29 09:12:06 +0900

    feat: 회원 가입 기능ver 1.0

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,49 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styled from "styled-components";
 
 import Footer from "../components/Footer";
 import { StyledInputContainer } from "../components/ui/LoginInput";
 import { ButtonLogin } from "../components/ui/Button";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // firebase 회원가입 코드
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+  
+  const emailChange = (e) => {
+    console.log(e)
+    setEmail(e);
+  }
+
+  const passwordChange = (e) => {
+    console.log(e)
+    setPassword(e);
+  }
+
+  const snsLoginHandler = (e) => {
+    e.preventDefault();
+    console.log(e)
+  };
+
   return (
     <>
       <StyledSignUpContainer>
         <StyledSignUpTitle>StudyHaza</StyledSignUpTitle>
-        <StyledInputContainer>
-          <label htmlFor="email">이메일 계정</label>
-          <div>
-            <input id="email" placeholder="이메일 계정" />
-            <i className="fa-solid fa-at" />
-          </div>
-        </StyledInputContainer>
-        <StyledInputContainer>
-          <label htmlFor="password">비밀번호</label>
-          <div>
-            <input id="password" type="password" placeholder="비밀번호" />
-            <i className="fa-solid fa-lock"></i>
-          </div>
-        </StyledInputContainer>
-        <StyledInputContainer>
-          <label htmlFor="passwordCheck">비밀번호 확인</label>
-          <div>
-            <input
-              id="passwordCheck"
-              type="password"
-              placeholder="비밀번호 확인"
-            />
-            <i className="fa-solid fa-lock"></i>
-          </div>
-        </StyledInputContainer>
-        <StyledInputContainer>
-          <label htmlFor="nickName">닉네임</label>
-          <div>
-            <input id="nickName" placeholder="사용할 닉네임" />
-            <i className="fa-solid fa-user"></i>
-          </div>
-        </StyledInputContainer>
-        <ButtonLogin>가입하기</ButtonLogin>
+        <form>
+          <StyledInputContainer>
+            <label htmlFor="email">이메일 계정</label>
+            <div>
+              <input id="email" type="email" placeholder="이메일 계정" onChange={e => emailChange(e.target.value)}/>
+              <i className="fa-solid fa-at" />
+            </div>
+          </StyledInputContainer>
+          <StyledInputContainer>
+            <label htmlFor="password">비밀번호</label>
+            <div>
+              <input id="password" type="password" placeholder="비밀번호" onChange={e => passwordChange(e.target.value)}/>
+              <i className="fa-solid fa-lock"></i>
+            </div>
+          </StyledInputContainer>
+          <StyledInputContainer>
+            <label htmlFor="passwordCheck">비밀번호 확인</label>
+            <div>
+              <input
+                id="passwordCheck"
+                type="password"
+                placeholder="비밀번호 확인"
+              />
+              <i className="fa-solid fa-lock"></i>
+            </div>
+          </StyledInputContainer>
+          <StyledInputContainer>
+            <label htmlFor="nickName">닉네임</label>
+            <div>
+              <input id="nickName" placeholder="사용할 닉네임" />
+              <i className="fa-solid fa-user"></i>
+            </div>
+          </StyledInputContainer>
+          <button type="submit" onClick={e => snsLoginHandler(e)}>가입하기</button>
+          <ButtonLogin>가입하기</ButtonLogin>
+        </form>
       </StyledSignUpContainer>
       <Footer />
     </>
@@ -66,6 +102,12 @@ const StyledSignUpContainer = styled.div`
     border: 0.5px solid #dedede;
     margin-top: 2rem;
     margin-bottom: 2rem;
+  }
+
+  form {
+    margin: 0;
+    padding: 0;
+    width: 100%;
   }
 `;
 
