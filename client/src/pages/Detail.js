3cad833a573c23f81e0a9fb36df99d35f289e2cd
Choi Soobin <64299610+Beanxx@users.noreply.github.com>
2022-10-24 01:16:31 +0900

    feat: 스터디모집 글 삭제 기능 구현 (#87)

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ViewStudy from "../components/ViewStudy";
 import { ButtonSecondary } from "../components/ui/Button";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import ViewCommunity from "../components/ViewCommunity";
+import {
+  deleteDoc,
+  query,
+  collection,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../firebase-config";
+import { useParams } from "react-router-dom";
+import Modal from "../components/Modal";
 
 function Detail() {
+  const { id } = useParams();
   const navigate = useNavigate();
 
   const pathName = useLocation().pathname.split("/")[1];
+
+  const [isOpenOk, setIsOpenOk] = useState(false);
+
+  const openModalHandler = () => {
+    setIsOpenOk(!isOpenOk);
+  };
+
+  const deletePost = async () => {
+    const q = query(collection(db, "posts"), where("id", "==", id));
+    const data = await getDocs(q);
+    try {
+      if (data.docs.length !== 0) {
+        await deleteDoc(data.docs[0].ref);
+      }
+      navigate("/");
+      window.location.reload();
+    } catch {
+      console.log("Not delete");
+    }
+  };
 
   return (
     <Container>
@@ -27,7 +58,16 @@ function Detail() {
         </ButtonSecondary>
         <ButtonRightBox>
           <ButtonSecondary>수정</ButtonSecondary>
-          <ButtonSecondary>삭제</ButtonSecondary>
+          <ButtonSecondary onClick={openModalHandler}>삭제</ButtonSecondary>
+          {isOpenOk && (
+            <Modal
+              isOpen={isOpenOk}
+              handleModal={deletePost}
+              setIsOpen={setIsOpenOk}
+            >
+              정말로 글을 삭제하시겠습니까?
+            </Modal>
+          )}
         </ButtonRightBox>
       </ButtonContainer>
     </Container>
