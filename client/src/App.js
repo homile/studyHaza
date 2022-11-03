c77fc151c6c1d1d21e91d4bbf04b52b826c1daf8
homile <ssanho2@naver.com>
2022-11-03 23:49:28 +0900

    chore:ts 마이그레이션 초기세팅 프리티어, eslintrc, tsconfig.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyle from "./GlobalStyle";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
