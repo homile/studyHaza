5f8db8d2b4ef2fd9de1596642ca0ee6b1b96a738
jin <jin@mycroft.kr>
2022-11-10 19:34:09 +0900

    feat. main.tsx 변환 (..)

--- client/src/components/UI/AdArea.tsx
+++ client/src/components/UI/AdArea.tsx
@@ -1,36 +1,43 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import CountUp from 'react-countup';
 
 interface Props {
-    posts: (posts: object) => void;
+  posts: [];
 }
 
-export const AdArea = ({posts} : Props) => {
-    return(
-        <BannerArea>
-            <p><strong><CountUp duration={1} start={0} end={posts.length} /></strong>개의 스터디가<br/>당신의 합류를 기다리는 중!</p>
-        </BannerArea>
-    )
+export const AdArea = ({ posts }: Props) => {
+  return (
+    <BannerArea>
+      <p>
+        <strong>
+          <CountUp duration={1} start={0} end={posts.length} />
+        </strong>
+        개의 스터디가
+        <br />
+        당신의 합류를 기다리는 중!
+      </p>
+    </BannerArea>
+  );
 };
 
 const BannerArea = styled.div`
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width:100%;
-    height:358px;
-    background:#F5F5F5;
-    border-radius: 25px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 358px;
+  background: #f5f5f5;
+  border-radius: 25px;
 
-    p{
-        font-size:50px;
-        font-family:'Pretendard-Bold';
-        text-align: center;
-        line-height: 1.1em;
+  p {
+    font-size: 50px;
+    font-family: 'Pretendard-Bold';
+    text-align: center;
+    line-height: 1.1em;
 
-        strong {
-            font-size:58px
-        }
+    strong {
+      font-size: 58px;
     }
-`
+  }
+`;
