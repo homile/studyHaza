13b5de8f75e00aab8c5ff028ac4607c8afa43a37
homile <ssanho2@naver.com>
2022-11-17 00:05:47 +0900

    chore:assets 이미지 임포트 모음

---
+++ client/src/assets/index.ts
@@ -0,0 +1,6 @@
+import naver_symbol from './naver_symbol.png';
+import facebook_symbol from './facebook_symbol.png';
+import kakao_symbol from './kakao_symbol.png';
+import google_symbol from './google_symbol.png';
+
+export { naver_symbol, facebook_symbol, kakao_symbol, google_symbol };
