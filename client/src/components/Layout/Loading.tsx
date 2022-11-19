e5dea0cb861e83d8a7d8c1083f2edeaf6deea8a5
Jin <laonjay@gmail.com>
2022-11-19 16:04:13 +0900

    chore. image.d gif추가 (#117)
    * chore. 멍멍 제거
    
    * chore. image.d gif추가

--- client/src/components/Layout/Loading.tsx
+++ client/src/components/Layout/Loading.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import img from '../../images/spinner.gif';
 export const Loading = () => {
-  const spinner = document.querySelector('loading-spinner');
-  if (spinner instanceof HTMLImageElement) {
-    spinner.src = '../../images/spinner.gif';
-  }
   return (
     <LoadingArea>
       <p>
-        <img className="loading-spinner" alt="" />
+        <img src={img} className="loading-spinner" alt="" />
       </p>
     </LoadingArea>
   );
